8ec94f1006521d5b3737e9ae95acabf8e7faeac2
Nicolas Schoonbroodt <nicolas@elsewhere.be>
2011-02-04 19:13:06 +0100

    Remove unused constructor

--- qt-driver/E60KdbDriver/e60kbdplugin.h
+++ qt-driver/E60KdbDriver/e60kbdplugin.h
@@ -6,8 +6,6 @@
 class E60KbdPlugin : public QKbdDriverPlugin {
     Q_OBJECT
 public:
-    E60KbdPlugin( QObject *parent  = 0 );
-    ~E60KbdPlugin();
     QWSKeyboardHandler* create(const QString& driver, const QString& device);
     QStringList keys() const;
 };
